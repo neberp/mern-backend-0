449d764b9d70f9dff75a8343b7a307a4ec7d1620
prebenn <42801690+prebenn@users.noreply.github.com>
2021-03-20 02:55:21 +0100

    Correct error print

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 function auth(req, res, next) {
   const token = req.header("x-auth-token");
-  if (!token) return res.status(401).send("Access denied. Not authorized.");
+  if (!token) return res.status(401).send("401 Unauthorized");
   try {
     const jwtSecretKey = process.env.CRUD_APP_JWT_SECRET_KEY;
     const decoded = jwt.verify(token, jwtSecretKey);
     req.user = decoded;
     next();
   } catch (ex) {
-    res.status(400).send("Invalid auth token.");
+    res.status(400).send("400 Bad Request");
   }
 }
 
